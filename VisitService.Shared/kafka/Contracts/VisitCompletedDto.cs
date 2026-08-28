ccd3e5c1cd98441bf1ed118657e5bbe8bbcf1836
Mirko627 <mirkoferrarimirkoferrari@gmail.com>
2026-08-28 14:50:56 +0200

    Add Shared/kafka

---
+++ VisitService.Shared/kafka/Contracts/VisitCompletedDto.cs
@@ -0,0 +1,10 @@
+﻿namespace VisitService.Shared.kafka.Contracts
+{
+    public class VisitCompletedDto
+    {
+        public required int PropertyId { get; set; }
+        public DateTime VisitDate { get; set; }
+        public int VisitatorId { get; set; }
+        public int OwnerId { get; set; }
+    }
+}
